56f933a3420dceee1beba79519253a544e8edcf1
nicolaananda <141715770+nicolaananda@users.noreply.github.com>
2024-06-04 11:33:59 +0700

    Product service 3 install express and add readme (#10)
    * Add deps, and first run
    
    * Change Active port to : 8003 (productService)
    
    ---------
    
    Co-authored-by: Indra Zulfi Mushoddaq <114550938+indrazm@users.noreply.github.com>

--- productService/src/index.ts
+++ productService/src/index.ts
@@ -4,6 +4,6 @@ const app = express();
 
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running at http://localhost:3000");
+app.listen(8003, "0.0.0.0", () => {
+  console.log("Server running at http://localhost:8003");
 });
