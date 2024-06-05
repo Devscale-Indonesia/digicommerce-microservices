7fce727cdd3596eaee9c5626394601254cda906b
Indra Zulfi <me@indrazm.com>
2024-06-06 00:33:30 +0700

    add routes to each services

--- apiGateway/src/index.ts
+++ apiGateway/src/index.ts
@@ -4,9 +4,57 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 const app = express();
 
 app.use(
+  "/api/auth",
+  createProxyMiddleware({
+    target: "http://localhost:8001",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/api/users",
+  createProxyMiddleware({
+    target: "http://localhost:8002",
+    changeOrigin: true,
+  })
+);
+
+app.use(
   "/api/products",
   createProxyMiddleware({
     target: "http://localhost:8003",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/api/search",
+  createProxyMiddleware({
+    target: "http://localhost:8004",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/api/carts",
+  createProxyMiddleware({
+    target: "http://localhost:8005",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/api/orders",
+  createProxyMiddleware({
+    target: "http://localhost:8006",
+    changeOrigin: true,
+  })
+);
+
+app.use(
+  "/api/reviews",
+  createProxyMiddleware({
+    target: "http://localhost:8007",
     changeOrigin: true,
   })
 );
