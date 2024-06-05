fd0fd2921f04f92ee2296e6fabf227f638c0dcb2
Nicola Ananda Dwi Ervantoro <nicolaanandadwiervantoro@gmail.com>
2024-06-05 21:19:21 +0700

    add model, controller, router and update index

--- productService/src/index.ts
+++ productService/src/index.ts
@@ -1,19 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import productRouter from "./router/productRouters";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URL!)
-  .then(() => console.log("connected to mongo"))
-  .catch(() => console.error("Monggo connection error"));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 
-// app.use("/product");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use("/products", productRouter);
 
 app.get("/", (req, res) => res.json({ message: "Hello World!" }));
 
 app.listen(8003, "0.0.0.0", () => {
-  console.log("Server running at http://localhost:8003");
+  console.log(`Server running at http://localhost:8003`);
 });
